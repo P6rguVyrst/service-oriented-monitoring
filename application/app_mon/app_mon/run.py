65e458afd90cd18e6f3c7ea3b7396c8aaf617482
Toomas Ormisson <toomas.ormisson@gmail.com>
2019-05-28 23:39:50 +0300

    resuming work on demo app.

--- application/app_mon/app_mon/run.py
+++ application/app_mon/app_mon/run.py
@@ -3,10 +3,10 @@ from prometheus_client import make_wsgi_app
 from werkzeug.wsgi import DispatcherMiddleware
 from app_mon.views import app_api, monitoring_api
 from app_mon.monitoring import monitor_request
-from logging.config import fileConfig
+from logging import config
 import logging
 
-fileConfig("local.cfg")
+config.fileConfig("local.cfg")
 logger = logging.getLogger("app_mon")
 logger.debug("Testing 123")
 
